2cb9fe869ef19e272c6b4a54bdbf73ff63646fb5
Thomas Shelby <onlyshelbythomas@gmail.com>
2022-02-19 11:10:18 +0700

    change deploy market

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,13 @@
 const {deployProxy, upgradeProxy}   = require('@openzeppelin/truffle-upgrades');
 const Nft                           = artifacts.require('Nft');
+const Market                        = artifacts.require('Market');
 
 module.exports = async function (deployer, network, accounts) {
     console.log('Nft deploy information:', network, accounts[0]);
 
     const nftInstance = await deployProxy(Nft, {deployer});
     console.table({Nft: nftInstance.address})
+
+    const marketInstance = await deployProxy(Market, [nftInstance.address], {deployer})
+    console.table({Market: marketInstance.address})
 }
