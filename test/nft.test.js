4e5440617e7819f3dd2c42718f2e8482f990c555
Thomas Shelby <onlyshelbythomas@gmail.com>
2022-02-19 17:33:08 +0700

    test success nft

--- test/nft.test.js
+++ test/nft.test.js
@@ -1,13 +1,13 @@
 require('chai').use(require('chai-as-promised')).should();
 
-const Nft = artifacts.require("Nft") ;
+const Nft = artifacts.require("Nft");
 let nft;
 
 before(async () => {
     nft = await Nft.deployed();
 })
 
-contract("Nft" , (accounts) => {
+contract("Nft", (accounts) => {
 
     describe('deployment', async () => {
         it('deploys successfully', async () => {
@@ -19,14 +19,33 @@ contract("Nft" , (accounts) => {
             assert.notEqual(address, undefined)
         })
 
-        it("Initial Success" , async () => {
-            const name   = await nft.name()
+        it("Initial Success", async () => {
+            const name = await nft.name()
             const symbol = await nft.symbol()
-            const owner  = await nft.owner()
+            const owner = await nft.owner()
 
             assert.equal(name, "Robox")
             assert.equal(symbol, "RBX")
             assert.equal(owner, accounts[0])
         });
     })
+
+    describe('Test Feature', async () => {
+        it("Mint and burn NFT", async () => {
+            const mintResult = await nft.mint(accounts[0], 'url')
+            const tokenId = mintResult.logs[0].args.tokenId.toNumber()
+            const url = await nft.tokenURI(tokenId)
+            let balance = await nft.balanceOf(accounts[0])
+
+            assert.equal(1, tokenId)
+            assert.equal(1, balance.toNumber())
+            assert.equal(url, "url")
+
+            await nft.burn(tokenId)
+            balance = await nft.balanceOf(accounts[0])
+
+            assert.equal(0, balance.toNumber())
+        })
+    })
+
 });
